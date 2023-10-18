d22f4018ac31f2ebacc8cf1dcb301acaa631db0a
HalefCaetano <halef_caetano@hotmail.com>
2023-10-18 08:36:05 -0700

    First Program

---
+++ Wokspace1/FirstProgram/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int favoriteNumber;
+    
+    cout << "Enter your favorite number betwwen 1 and 100:" << endl;
+    cin >> favoriteNumber;
+    cout << "Amazing! That's my favorite number too." << endl;
+    cout << "No really! " << favoriteNumber <<  " is my favorite number." << endl;
+    
+    return 0;
+    }
